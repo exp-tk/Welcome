8f17e7cd020db86b3cf2b7a83f166d5975c803a2
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2024-02-08 10:02:23 +0900

    Welcomeからマイナスマージンを消す

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -35,7 +35,7 @@ export const Header = () => {
     }`;
 
   return (
-    <header className="sticky -mb-16 top-0 flex items-center px-4 md:px-8 py-4 shadow w-full bg-background z-50 justify-between md:gap-16">
+    <header className="sticky -mb-24 top-0 flex items-center px-4 md:px-8 py-4 shadow w-full bg-background z-50 justify-between md:gap-16">
       <Link to={PAGE_SECTION.Welcome} smooth duration={500}>
         <div className="flex">
           <TKSymbol width={32} height={32} />
