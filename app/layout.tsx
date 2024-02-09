b396709b50dab85ca81e1015eee2794a63664a1b
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2024-02-09 21:16:57 +0900

    favicon設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter_Tight } from "next/font/google";
+import Head from "next/head";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import "./globals.css";
@@ -21,6 +22,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja" className="text-foreground bg-background">
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#212121" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <body className="min-h-screen w-screen overflow-x-hidden">
         <Providers>
           <main className={interTight.className}>
