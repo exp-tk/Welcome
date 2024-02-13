1bd24275d9602ba032219b2f75899d56363e5cb0
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2024-02-13 21:28:22 +0900

    変な置き方だったアイコンを移動

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter_Tight } from "next/font/google";
-import Head from "next/head";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import "./globals.css";
@@ -22,29 +21,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja" className="text-foreground bg-background">
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#212121" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
       <body className="min-h-screen w-screen overflow-x-hidden">
         <Providers>
           <main className={interTight.className}>
